4e487b06bb2db0e626e0de01af6307e47ba0a40d
Schaffer Stewart <schaestewart@gmail.com>
2021-08-01 15:45:11 -0400

    Set up login/logout form and handlers

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/SchaeStewart/ssrWebComponents
 
-go 1.14
+go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"embed"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//go:embed index.html
+var indexHTML embed.FS
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("error"))
+		return
+	}
+	username := r.FormValue("username")
+
+	http.SetCookie(w, &http.Cookie{
+		Name:  "username",
+		Value: username,
+	})
+	http.Redirect(w, r, r.Header.Get("Referer"), http.StatusFound)
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:   "username",
+		Value:  "",
+		MaxAge: -1,
+	})
+	http.Redirect(w, r, r.Header.Get("Referer"), http.StatusFound)
+}
 
 func main() {
-	fmt.Println("hello world")
+	r := mux.NewRouter()
+
+	r.Handle("/", http.FileServer(http.FS(indexHTML)))
+	r.HandleFunc("/login", Login).Methods("POST")
+	r.HandleFunc("/logout", Logout).Methods("POST")
+
+	log.Println("server starting on localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
